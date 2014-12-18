6e94541bce55fd17f719df98a176b72c61cc6238
Nicolás C. <crevs.daak@gmail.com>
2014-12-18 18:48:18 -0300

    Fixed action identifier

--- d/party_dialog.d
+++ d/party_dialog.d
@@ -2,7 +2,7 @@ BEGIN "7C#GALP"
 
 IF  ~Global("7C#GalileoMelicampComment","GLOBAL",2)~ BEGIN 7c#melicamp.joke.1
   SAY @53 /* Hey <CHARNAME>, I hear Feldepost fetches a fair price for fresh poultry... Only joking. */
-    IF ~~ DO ~Global("7C#GalileoMelicampComment","GLOBAL",3)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoMelicampComment","GLOBAL",3)~ EXIT
 END
 
 IF ~Global("7C#GalileoDeadHalflingComment","GLOBAL",1)~ BEGIN 7c#galileo.sees.dead.halflings
