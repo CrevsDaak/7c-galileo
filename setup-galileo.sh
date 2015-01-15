370fe90ddb4923d2c84feff628ef14d507d84113
Crevs Daak <crevs.daak@gmail.com>
2015-01-15 20:37:22 -0300

    if usr's using OSX don't point to Linux WeiDU
    I'm such an idiot!

--- setup-galileo.sh
+++ setup-galileo.sh
@@ -6,9 +6,9 @@ c_p=${0%/*}
 cd "$c_p" && cd ..
 
 if [[ "$OSTYPE" =~ "darwin" ]]; then
-  WEIDU_EXEC = "weidu_linux"
-else
   WEIDU_EXEC = "weidu_osx"
+else
+  WEIDU_EXEC = "weidu_linux"
 fi
 
 ./"$WEIDU_EXEC" --log setup-7c-galileo.debug  7c-galileo/7c-galileo.tp2
