37743eb4c7a43f3b39c654254ebd2e7adc8b3302
Nicolás C <crevs.daak@gmail.com>
2015-02-05 17:44:33 -0300

    turn unnecessary globals into locals
    ... and add a joining back dialog!

--- d/before_joining.d
+++ d/before_joining.d
@@ -2,16 +2,16 @@ BEGIN "7C#GAL1P"
 
 IF ~!NumTimesTalkedTo(0)
 !InParty("Galileo1")
-Global("7C#GalileoInParty","GLOBAL",1)~ BEGIN 7c#meet.after.kicking.1
-  SAY ~DO YOU WANT ME BACK ON YOUR PARTY?~
-    IF ~~ REPLY ~YES.~ DO ~JoinParty()
-SetGlobal("7C#GalileoInParty","GLOBAL",2)~ EXIT
-    IF ~~ REPLY ~NO.~ DO ~~ EXIT
+Global("7C#GalileoInParty","LOCALS",1)~ BEGIN 7c#meet.after.kicking.1
+  SAY @55 /* Fancy seeing you here. Starting to miss my company, I hope? I know I've missed yours', my friend. */
+    IF ~~ REPLY @56 /* Yes, Galileo, I would like to take you back in. */ GOTO 7c#join.after.kicking.1
+    IF ~~ REPLY @57 /* I just thought I'd say hello and check on how you're doing. I do not need your services at the moment. */ GOTO 7c#sorry.next.time.1
+    IF ~~ REPLY @58 /* No, I must be on my way. */ GOTO 7c#not.this.time.galileo.1
 END
 
 IF ~NumTimesTalkedTo(0)
 !Global("7C#DoneFirewineRuins","GLOBAL",1)
-!Global("7C#GalileoInParty","GLOBAL",1)~ BEGIN 7c#meeting.1
+!Global("7C#GalileoInParty","LOCALS",1)~ BEGIN 7c#meeting.1
   SAY @1 /* Greetings, I am called Galileo; bard, servant of Finder Wyvernspur, and I am at your service. */
     IF ~~ REPLY @2 /* Hello there my small friend, I am <CHARNAME> */ GOTO 7c#meeting.2
     IF ~~ REPLY @3 /* No, I do not like you. */ GOTO 7c#end.angry.1
@@ -21,16 +21,16 @@ END
 
 IF ~NumTimesTalkedTo(0)
 Global("7C#DoneFirewineRuins","GLOBAL",1)
-!Global("7C#GalileoInParty","GLOBAL",1)~ BEGIN 7c#meet.firewine.done.1
+!Global("7C#GalileoInParty","LOCALS",1)~ BEGIN 7c#meet.firewine.done.1
   SAY @44 /* Well, it's nice to finally meet the savior of our little town! */
     IF ~~ REPLY @45 /* Hello there my small friend. */ GOTO 7c#hey.there.shorty
     IF ~~ REPLY @5 /* Please excuse me, I haven't the time to speak with you. */ GOTO 7c#happy.bye.1
 END
 
 IF ~!NumTimesTalkedTo(0)
-GlobalGT("7C#GalileoHappiness1","GLOBAL",9)
+GlobalGT("7C#GalileoHappiness","LOCALS",9)
 !Global("7C#DoneFirewineRuins","GLOBAL",1)
-!Global("7C#GalileoInParty","GLOBAL",1)~ BEGIN 7c#meet.2.happy
+!Global("7C#GalileoInParty","LOCALS",1)~ BEGIN 7c#meet.2.happy
   SAY @22 /* Looking to have a helper for exploring the Firewine Ruins? */
     IF ~~ REPLY @23 /* Yes, I would be pleased to have you join me. */ GOTO 7c#meet.2.happy.join.1
     IF ~~ REPLY @3 /* No, I do not like you */ GOTO 7c#end.angry.1
@@ -39,9 +39,9 @@ GlobalGT("7C#GalileoHappiness1","GLOBAL",9)
 END
 
 IF ~!NumTimesTalkedTo(0)
-GlobalLT("7C#GalileoHappiness1","GLOBAL",10)
+GlobalLT("7C#GalileoHappiness","LOCALS",10)
 !Global("7C#DoneFirewineRuins","GLOBAL",1)
-!Global("7C#GalileoInParty","GLOBAL",1)~ BEGIN 7c#meet.angry.1
+!Global("7C#GalileoInParty","LOCALS",1)~ BEGIN 7c#meet.angry.1
   SAY @28 /* Hmm? Oh, what do you want? */
     IF ~~ REPLY @29 /* I was wondering if you could join my group in our venture in the Firewine Ruins. */ GOTO 7c#meet.angry.joins.1
     IF ~~ REPLY @30 /* I just wanted to say that I'm sorry for what I said before. */ GOTO 7c#sorry.bro.1
@@ -49,9 +49,31 @@ GlobalLT("7C#GalileoHappiness1","GLOBAL",10)
 END
 
 IF ~!NumTimesTalkedTo(0)
-GlobalLT("7C#GalileoHappiness1","GLOBAL",5)
-!Global("7C#GalileoInParty","GLOBAL",1)~ BEGIN 7c#very.angry
+GlobalLT("7C#GalileoHappiness","LOCALS",5)
+!Global("7C#GalileoInParty","LOCALS",1)~ BEGIN 7c#very.angry
   SAY @43 /* Bugger off. */
+    IF ~~ DO ~~ EXIT
+END
+
+IF ~~ BEGIN 7c#join.after.kicking.1
+  SAY @59 /* Excellent! Let's be on our way! */
+    IF ~~ DO ~JoinParty()
+SetGlobal("7C#GalileoInParty","LOCALS",2)~ EXIT
+END
+
+IF ~~ BEGIN 7c#sorry.next.time.1
+  SAY @60 /* Galileo: Yes, you don't need them, but do you want them? */
+    IF ~~ REPLY @61 /* Quite amusing, Galileo. Why not, welcome aboard. */ GOTO 7c#join.after.kicking.1
+    IF ~~ REPLY @62 /* Very funny, but no, I must be off. */ GOTO 7c#i.will.level.up.and.get.in.ur.group
+END
+
+IF ~~ BEGIN 7c#not.this.time.galileo.1
+  SAY @63 /* What a tease! I'll win myself back into your party yet, just you wait. */
+    IF ~~ DO ~~ EXIT
+END
+
+IF ~~ BEGIN 7c#i.will.level.up.and.get.in.ur.group
+  SAY @64 /* I suppose it will take more than a witty line or two to win myself back into your group. Fair [...] */
     IF ~~ DO ~~ EXIT
 END
 
@@ -63,23 +85,23 @@ END
 
 IF ~~ BEGIN 7c#end.angry.1
   SAY @9 /* Oh. */
-    IF ~~ DO ~SetGlobal("7C#GalileoHappiness1","GLOBAL",5)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoHappiness","LOCALS",5)~ EXIT
 END
 
 IF ~~ BEGIN 7c#joins.1
   SAY @10 /* Excellent. To the Firewine then. Let us be off! */
     IF ~~ DO ~JoinParty()
-SetGlobal("7C#GalileoInParty","GLOBAL",2)~ EXIT
+SetGlobal("7C#GalileoInParty","LOCALS",2)~ EXIT
 END
 
 IF ~~ BEGIN 7c#happy.bye.1
   SAY @11 /* No trouble at all, good sir. Come back if you need me, I likely won't be going anywhere. */
-    IF ~~ DO ~SetGlobal("7C#GalileoHappiness1","GLOBAL",15)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoHappiness","LOCALS",15)~ EXIT
 END
 
 IF ~~ BEGIN 7c#end.angry.2
   SAY @12 /* Well that was uncalled for. Bugger off, rude one. */
-    IF ~~ DO ~SetGlobal("7C#GalileoHappiness1","GLOBAL",5)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoHappiness","LOCALS",5)~ EXIT
 END
 
 IF ~~ BEGIN 7c#answers.1
@@ -98,35 +120,35 @@ END
 IF ~~ BEGIN 7c#joins.sarcasm.1
   SAY @19 /* I hope you are not being sarcastic, my friend. */
     IF ~~ DO ~JoinParty()
-SetGlobal("7C#GalileoInParty","GLOBAL",2)~ EXIT
+SetGlobal("7C#GalileoInParty","LOCALS",2)~ EXIT
 END
 
 IF ~~ BEGIN 7c#marvelous.joins.1
   SAY @20 /* Oh marvelous, just mah-velous! */
     IF ~~ DO ~JoinParty()
-SetGlobal("7C#GalileoInParty","GLOBAL",2)~ EXIT
+SetGlobal("7C#GalileoInParty","LOCALS",2)~ EXIT
 END
 
 IF ~~ BEGIN 7c#happy.good.bye.1
   SAY @21 /* You make me sad. So be it. */
-    IF ~~ DO ~SetGlobal("7C#GalileoHappiness1","GLOBAL",15)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoHappiness","LOCALS",15)~ EXIT
 END
 
 IF ~~ BEGIN 7c#meet.2.happy.join.1
   SAY @26 /* Joyous day! Most excellent! */
     IF ~~ DO ~JoinParty()
-SetGlobal("7C#GalileoInParty","GLOBAL",2)~ EXIT
+SetGlobal("7C#GalileoInParty","LOCALS",2)~ EXIT
 END
 
 IF ~~ BEGIN 7c#happy.bye.again.1
   SAY @27 /* No trouble. */
-    IF ~~ DO ~SetGlobal("7C#GalileoHappiness1","GLOBAL",15)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoHappiness","LOCALS",15)~ EXIT
 END
 
 IF ~~ BEGIN 7c#meet.angry.joins.1
   SAY @32 /* Just like that? Here I thought you were not nice at all. All right then, let us be off. */
     IF ~~ DO ~JoinParty()
-SetGlobal("7C#GalileoInParty","GLOBAL",2)~ EXIT
+SetGlobal("7C#GalileoInParty","LOCALS",2)~ EXIT
 END
 
 IF ~~ BEGIN 7c#sorry.bro.1
@@ -145,12 +167,12 @@ END
 
 IF ~~ BEGIN 7c#fuck.you
   SAY @41 /* Well, I never! */
-    IF ~~ DO ~SetGlobal("7C#GalileoHappiness1","GLOBAL",0)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoHappiness","LOCALS",0)~ EXIT
 END
 
 IF ~~ BEGIN 7c#oh.sorry
   SAY @42 /* Erm ... I see. Well, that's not exactly my type of humor, but okay. */
-    IF ~~ DO ~SetGlobal("7C#GalileoHappiness1","GLOBAL",15)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoHappiness","LOCALS",15)~ EXIT
 END
 
 IF ~~ BEGIN 7c#hey.there.shorty
@@ -162,15 +184,15 @@ END
 
 IF ~~ BEGIN 7c#no.place.4.ya
   SAY @49 /* What? How? Oi, I'm this small! Oh ... fine. */
-    IF ~~ DO ~SetGlobal("7C#GalileoHappiness1","GLOBAL",15)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoHappiness","LOCALS",15)~ EXIT
 END
 
 IF ~~ BEGIN 7c#meet.angry.happy.bye.1
   SAY @51 /* No trouble at all, good sir. Come back if you need me, I likely won't be going anywhere. */
-    IF ~~ DO ~SetGlobal("7C#GalileoHappiness1","GLOBAL",15)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoHappiness","LOCALS",15)~ EXIT
 END
 
 IF ~~ BEGIN 7c#no.prob.bob.bye
   SAY @52 /* That's no problem. Let me know if'n you change your mind, I'm bored. */
-    IF ~~ DO ~SetGlobal("7C#GalileoHappiness1","GLOBAL",15)~ EXIT
+    IF ~~ DO ~SetGlobal("7C#GalileoHappiness","LOCALS",15)~ EXIT
 END
