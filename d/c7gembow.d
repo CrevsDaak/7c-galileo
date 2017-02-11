b1b05d8f131faf3f7711035ff6402b8b7670e72f
Quartz <quartzlcc@yahoo.com>
2017-02-10 20:39:22 -0800

    Update c7gembow.d

--- d/c7gembow.d
+++ d/c7gembow.d
@@ -24,69 +24,69 @@ IF ~CombatCounter(0)~ BEGIN 3
 END
 
 IF ~CombatCounter(0)~ BEGIN 4
-  SAY @67 /* whihc jnsknfkjdnfskdm u want to insert bhaal's deep in ur gemcuntblow */
+  SAY @67 /* Which gem would you like to equip? */
     IF ~~ REPLY @69 GOTO 2
-    IF ~PartyHasItemIdentified("MISC19")~ REPLY @4 /* attach a Turquoise Gem */
+    IF ~PartyHasItemIdentified("MISC19")~ REPLY @4 /* Affix a Turquoise Gem to the indentation. */
        DO ~TakePartyItemNum("MISC19",1) DestroyItem("MISC19") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw01",LastTalkedToBy(Myself),0,0,0)~ GOTO 5
-    IF ~PartyHasItemIdentified("MISC16")~ REPLY @5 /* attach a Fire Agate Gem */
+    IF ~PartyHasItemIdentified("MISC16")~ REPLY @5 /* Affix a Fire Agate Gem to the indentation. */
        DO ~TakePartyItemNum("MISC16",1) DestroyItem("MISC16") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw02",LastTalkedToBy(Myself),0,0,0)~ GOTO 6
-    IF ~PartyHasItemIdentified("MISC17")~ REPLY @6 /* attach a Lynx Eye Gem */
+    IF ~PartyHasItemIdentified("MISC17")~ REPLY @6 /* Affix a Lynx Eye Gem to the indentation. */
        DO ~TakePartyItemNum("MISC17",1) DestroyItem("MISC17") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw03",LastTalkedToBy(Myself),0,0,0)~ GOTO 7
-    IF ~PartyHasItemIdentified("MISC18")~ REPLY @7 /* attach a Sunstone Gem */
+    IF ~PartyHasItemIdentified("MISC18")~ REPLY @7 /* Affix a Sunstone Gem to the indentation. */
        DO ~TakePartyItemNum("MISC18",1) DestroyItem("MISC18") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw04",LastTalkedToBy(Myself),0,0,0)~ GOTO 8
-    IF ~PartyHasItemIdentified("MISC22")~ REPLY @8 /* attach a Andar Gem */
+    IF ~PartyHasItemIdentified("MISC22")~ REPLY @8 /* Affix a Andar Gem to the indentation. */
        DO ~TakePartyItemNum("MISC22",1) DestroyItem("MISC22") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw05",LastTalkedToBy(Myself),0,0,0)~ GOTO 9
-    IF ~PartyHasItemIdentified("MISC23")~ REPLY @9 /* attach a Jasper Gem */
+    IF ~PartyHasItemIdentified("MISC23")~ REPLY @9 /* Affix a Jasper Gem to the indentation. */
        DO ~TakePartyItemNum("MISC23",1) DestroyItem("MISC23") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw06",LastTalkedToBy(Myself),0,0,0)~ GOTO 10
-    IF ~PartyHasItemIdentified("MISC24")~ REPLY @10 /* attach a Tchazar Gem */
+    IF ~PartyHasItemIdentified("MISC24")~ REPLY @10 /* Affix a Tchazar Gem to the indentation. */
        DO ~TakePartyItemNum("MISC24",1) DestroyItem("MISC24") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw07",LastTalkedToBy(Myself),0,0,0)~ GOTO 11
-    IF ~PartyHasItemIdentified("MISC26")~ REPLY @11 /* attach a Iol Gem */
+    IF ~PartyHasItemIdentified("MISC26")~ REPLY @11 /* Affix a Iol Gem to the indentation. */
        DO ~TakePartyItemNum("MISC26",1) DestroyItem("MISC26") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw08",LastTalkedToBy(Myself),0,0,0)~ GOTO 12
-    IF ~PartyHasItemIdentified("MISC21")~ REPLY @12 /* attach a Skydrop Gem */
+    IF ~PartyHasItemIdentified("MISC21")~ REPLY @12 /* Affix a Skydrop Gem to the indentation. */
        DO ~TakePartyItemNum("MISC21",1) DestroyItem("MISC21") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw09",LastTalkedToBy(Myself),0,0,0)~ GOTO 13
-    IF ~PartyHasItemIdentified("MISC25")~ REPLY @13 /* attach a Zircon Gem */
+    IF ~PartyHasItemIdentified("MISC25")~ REPLY @13 /* Affix a Zircon Gem to the indentation. */
        DO ~TakePartyItemNum("MISC25",1) DestroyItem("MISC25") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw10",LastTalkedToBy(Myself),0,0,0)~ GOTO 14
-    IF ~PartyHasItemIdentified("MISC20")~ REPLY @14 /* attach a Bloodstone Gem */
+    IF ~PartyHasItemIdentified("MISC20")~ REPLY @14 /* Affix a Bloodstone Gem to the indentation. */
        DO ~TakePartyItemNum("MISC20",1) DestroyItem("MISC20") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw11",LastTalkedToBy(Myself),0,0,0)~ GOTO 15
-    IF ~PartyHasItemIdentified("MISC27")~ REPLY @15 /* attach a Moonstone Gem */
+    IF ~PartyHasItemIdentified("MISC27")~ REPLY @15 /* Affix a Moonstone Gem to the indentation. */
        DO ~TakePartyItemNum("MISC27",1) DestroyItem("MISC27") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw12",LastTalkedToBy(Myself),0,0,0)~ GOTO 16
-    IF ~PartyHasItemIdentified("MISC29")~ REPLY @16 /* attach a Ziose Gem */
+    IF ~PartyHasItemIdentified("MISC29")~ REPLY @16 /* Affix a Ziose Gem to the indentation. */
        DO ~TakePartyItemNum("MISC29",1) DestroyItem("MISC29") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw13",LastTalkedToBy(Myself),0,0,0)~ GOTO 17
-    IF ~PartyHasItemIdentified("MISC30")~ REPLY @17 /* attach a Chrysoberyl Gem */
+    IF ~PartyHasItemIdentified("MISC30")~ REPLY @17 /* Affix a Chrysoberyl Gem to the indentation. */
        DO ~TakePartyItemNum("MISC30",1) DestroyItem("MISC30") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw14",LastTalkedToBy(Myself),0,0,0)~ GOTO 18
-    IF ~PartyHasItemIdentified("MISC28")~ REPLY @18 /* attach a Waterstar Gem */
+    IF ~PartyHasItemIdentified("MISC28")~ REPLY @18 /* Affix a Waterstar Gem to the indentation. */
        DO ~TakePartyItemNum("MISC28",1) DestroyItem("MISC28") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw15",LastTalkedToBy(Myself),0,0,0)~ GOTO 19
-    IF ~PartyHasItemIdentified("MISC32")~ REPLY @19 /* attach a Shandon Gem */
+    IF ~PartyHasItemIdentified("MISC32")~ REPLY @19 /* Affix a Shandon Gem to the indentation. */
        DO ~TakePartyItemNum("MISC32",1) DestroyItem("MISC32") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw16",LastTalkedToBy(Myself),0,0,0)~ GOTO 20
-    IF ~PartyHasItemIdentified("MISC31")~ REPLY @20 /* attach a Star Diopside Gem */
+    IF ~PartyHasItemIdentified("MISC31")~ REPLY @20 /* Affix a Star Diopside Gem to the indentation. */
        DO ~TakePartyItemNum("MISC31",1) DestroyItem("MISC31") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw17",LastTalkedToBy(Myself),0,0,0)~ GOTO 21
-    IF ~PartyHasItemIdentified("MISC35")~ REPLY @21 /* attach a Horn Coral Gem */
+    IF ~PartyHasItemIdentified("MISC35")~ REPLY @21 /* Affix a Horn Coral Gem to the indentation. */
        DO ~TakePartyItemNum("MISC35",1) DestroyItem("MISC35") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw18",LastTalkedToBy(Myself),0,0,0)~ GOTO 22
-    IF ~PartyHasItemIdentified("MISC33")~ REPLY @22 /* attach a Aquamarine Gem */
+    IF ~PartyHasItemIdentified("MISC33")~ REPLY @22 /* Affix a Aquamarine Gem to the indentation. */
        DO ~TakePartyItemNum("MISC33",1) DestroyItem("MISC33") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw19",LastTalkedToBy(Myself),0,0,0)~ GOTO 23
-    IF ~PartyHasItemIdentified("MISC34")~ REPLY @23 /* attach a Garnet Gem */
+    IF ~PartyHasItemIdentified("MISC34")~ REPLY @23 /* Affix a Garnet Gem to the indentation. */
        DO ~TakePartyItemNum("MISC34",1) DestroyItem("MISC34") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw20",LastTalkedToBy(Myself),0,0,0)~ GOTO 24
-    IF ~PartyHasItemIdentified("MISC36")~ REPLY @24 /* attach a Pearl */
+    IF ~PartyHasItemIdentified("MISC36")~ REPLY @24 /* Affix a Pearl to the indentation. */
        DO ~TakePartyItemNum("MISC36",1) DestroyItem("MISC36") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw21",LastTalkedToBy(Myself),0,0,0)~ GOTO 25
-    IF ~PartyHasItemIdentified("MISC37")~ REPLY @25 /* attach a Sphene Gem */
+    IF ~PartyHasItemIdentified("MISC37")~ REPLY @25 /* Affix a Sphene Gem to the indentation. */
        DO ~TakePartyItemNum("MISC37",1) DestroyItem("MISC37") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw22",LastTalkedToBy(Myself),0,0,0)~ GOTO 26
-    IF ~PartyHasItemIdentified("MISC38")~ REPLY @26 /* attach a Black Opal */
+    IF ~PartyHasItemIdentified("MISC38")~ REPLY @26 /* Affix a Black Opal to the indentation. */
        DO ~TakePartyItemNum("MISC38",1) DestroyItem("MISC38") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw23",LastTalkedToBy(Myself),0,0,0)~ GOTO 27
-    IF ~PartyHasItemIdentified("MISC39")~ REPLY @27 /* attach a Water Opal */
+    IF ~PartyHasItemIdentified("MISC39")~ REPLY @27 /* Affix a Water Opal to the indentation. */
        DO ~TakePartyItemNum("MISC39",1) DestroyItem("MISC39") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw24",LastTalkedToBy(Myself),0,0,0)~ GOTO 28
-    IF ~PartyHasItemIdentified("MISC40")~ REPLY @28 /* attach a Moonbar Gem */
+    IF ~PartyHasItemIdentified("MISC40")~ REPLY @28 /* Affix a Moonbar Gem to the indentation. */
        DO ~TakePartyItemNum("MISC40",1) DestroyItem("MISC40") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw25",LastTalkedToBy(Myself),0,0,0)~ GOTO 29
-    IF ~PartyHasItemIdentified("MISC42")~ REPLY @29 /* attach a Diamond */
+    IF ~PartyHasItemIdentified("MISC42")~ REPLY @29 /* Affix a Diamond to the indentation. */
        DO ~TakePartyItemNum("MISC42",1) DestroyItem("MISC42") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw26",LastTalkedToBy(Myself),0,0,0)~ GOTO 30
-    IF ~PartyHasItemIdentified("MISC43")~ REPLY @30 /* attach a Emerald */
+    IF ~PartyHasItemIdentified("MISC43")~ REPLY @30 /* Affix a Emerald to the indentation. */
        DO ~TakePartyItemNum("MISC43",1) DestroyItem("MISC43") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw27",LastTalkedToBy(Myself),0,0,0)~ GOTO 31
-    IF ~PartyHasItemIdentified("MISC41")~ REPLY @31 /* attach a Star Sapphire */
+    IF ~PartyHasItemIdentified("MISC41")~ REPLY @31 /* Affix a Star Sapphire to the indentation. */
        DO ~TakePartyItemNum("MISC41",1) DestroyItem("MISC41") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw28",LastTalkedToBy(Myself),0,0,0)~ GOTO 32
-    IF ~PartyHasItemIdentified("MISC44")~ REPLY @32 /* attach a King's Tears */
+    IF ~PartyHasItemIdentified("MISC44")~ REPLY @32 /* Affix a King's Tears to the indentation. */
        DO ~TakePartyItemNum("MISC44",1) DestroyItem("MISC44") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw29",LastTalkedToBy(Myself),0,0,0)~ GOTO 33
-    IF ~PartyHasItemIdentified("MISC45")~ REPLY @33 /* attach a Rogue Stone */
+    IF ~PartyHasItemIdentified("MISC45")~ REPLY @33 /* Affix a Rogue Stone to the indentation. */
        DO ~TakePartyItemNum("MISC45",1) DestroyItem("MISC45") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw30",LastTalkedToBy(Myself),0,0,0)~ GOTO 34
-    IF ~PartyHasItemIdentified("MISC1J")~ REPLY @34 /* attach a Glittering Beljuril Gemstone */
+    IF ~PartyHasItemIdentified("MISC1J")~ REPLY @34 /* Affix a Glittering Beljuril Gemstone to the indentation. */
        DO ~TakePartyItemNum("MISC1J",1) DestroyItem("MISC1J") TakePartyItem("c7gembow") DestroyItem("c7gembow") GiveItemCreate("c7#gbw31",LastTalkedToBy(Myself),0,0,0)~ GOTO 35
 END
 
